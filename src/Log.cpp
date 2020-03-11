21e4c2bd8681802f2143e1d1de42193a38f3b7ad
BAN0008 <lindenbanner@protonmail.com>
2020-03-11 17:18:13 +1100

    Fix log

--- src/Log.cpp
+++ src/Log.cpp
@@ -8,41 +8,45 @@ namespace Yenah
 {
 	void Log::Info(const char *message, ...)
 	{
-		char *buffer = (char *)malloc(strlen(message) + strlen("Information: \n"));
+		char *buffer = (char *)malloc(strlen(message) + strlen("Information: \n") + 2);
 		sprintf(buffer, "Information: %s\n", message);
 		va_list args;
 		va_start(args, message);
 		vprintf(buffer, args);
 		va_end(args);
+		free(buffer);
 	}
 
 	void Log::Warn(const char *message, ...)
 	{
-		char *buffer = (char *)malloc(strlen(message) + strlen("Warning: \n"));
+		char *buffer = (char *)malloc(strlen(message) + strlen("Warning: \n") + 2);
 		sprintf(buffer, "Warning: %s\n", message);
 		va_list args;
 		va_start(args, message);
 		vprintf(buffer, args);
 		va_end(args);
+		free(buffer);
 	}
 
 	void Log::Error(const char *message, ...)
 	{
-		char *buffer = (char *)malloc(strlen(message) + strlen("Error: \n"));
+		char *buffer = (char *)malloc(strlen(message) + strlen("Error: \n") + 2);
 		sprintf(buffer, "Error: %s\n", message);
 		va_list args;
 		va_start(args, message);
 		vprintf(buffer, args);
 		va_end(args);
+		free(buffer);
 	}
 
 	void Log::Fatal(const char *message, ...)
 	{
-		char *buffer = (char *)malloc(strlen(message) + strlen("Fatal Error: \n"));
+		char *buffer = (char *)malloc(strlen(message) + strlen("Fatal Error: \n") + 2);
 		sprintf(buffer, "Fatal Error: %s\n", message);
 		va_list args;
 		va_start(args, message);
 		vprintf(buffer, args);
 		va_end(args);
+		free(buffer);
 	}	
 }
